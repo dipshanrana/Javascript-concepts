189359104523fb8252a7a8acc40351812f4cc806
dipshanrana <dipshanranabhat09@gmail.com>
2026-06-10 15:20:33 +0545

    typecasting

---
+++ javascript-learning/Typecasting.js
@@ -0,0 +1,55 @@
+// // let num = Number("1111")
+// // console.log(num)
+
+// // console.log("44"*"2")
+
+// // console.log(Number("hello"))
+
+// // let num1 = Number("hello")
+
+// // console.log(num1)
+
+// // console.log(Number(true))
+
+// // console.log(parseInt("222a"))
+
+// // console.log(parseInt("22.33"))
+
+
+// // let text = String(num)
+// // console.log(text)
+
+// console.log(Boolean(0))
+// console.log(Boolean(1))
+
+// console.log(Boolean(null))
+// console.log(Boolean(-1))
+
+// let a = "33"
+// let b = 4
+
+// console.log(a+b)
+
+// let c = a+b
+
+// console.log(typeof(c))
+
+// let d = b.toString();
+// console.log(d)
+
+
+// console.log("55"-33)
+
+// console.log(true+3)
+
+
+console.log(5=="5")
+console.log(5==="5")
+
+
+let a = String(333)
+console.log(a)
+
+let b = Number("okay")
+b = 3
+console.log(b)
