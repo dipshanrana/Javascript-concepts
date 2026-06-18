5162b598f7d7a61153f2c6771c0bc695c391b28a
dipshanrana <dipshanranabhat09@gmail.com>
2026-06-18 21:18:17 +0545

    iterations

--- javascript-learning/iteration.js
+++ javascript-learning/iteration.js
@@ -16,7 +16,38 @@
 
 //for loop
 
-for(let i = 0 ; i<5; i++){
-    console.log(i)
+// for(let i = 0 ; i<5; i++){
+//     console.log(i)
+// }
+
+// let i = 0;
+
+// while(i<5){
+//     console.log(i)
+//     i++;
+// }
+
+// do{
+//     console.log(i)
+//     i++;
+// }
+// while(i<7);
+
+const arr = [10,20,30,40]
+
+for(const value of arr){
+    console.log()
 }
 
+
+for(const char of "okay"){
+    console.log(char)
+}
+
+
+for(let j = 0 ; j<=5 ; j++){
+   if(j==1)
+    break;
+
+   console.log(j)
+}
