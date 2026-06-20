1d367c1bb8f7b107b7a1aa1ac305627779212a21
dipshanrana <dipshanranabhat09@gmail.com>
2026-06-20 13:04:29 +0545

    comparision operators

--- javascript-learning/expressionsandoperators.js
+++ javascript-learning/expressionsandoperators.js
@@ -1,32 +1,52 @@
-//arithmetic operators
+// //arithmetic operators
 
-console.log(5+3)
+// console.log(5+3)
 
-console.log("hello"+ "world")
+// console.log("hello"+ "world")
 
-console.log(10%3)
-
-
-num = "33"
-
-console.log(num%3===0)
+// console.log(10%3)
 
 
-let x = 1;
+// num = "33"
 
-x &&= 4;
-console.log(x)
-
-console.log("5"+5)
-
-console.log(10/0)
-console.log(Math.pow(2,3))
+// console.log(num%3===0)
 
 
-let a = true;
+// let x = 1;
 
-a &&= 4;
-console.log(a)
+// x &&= 4;
+// console.log(x)
+
+// console.log("5"+5)
+
+// console.log(10/0)
+// console.log(Math.pow(2,3))
+
+
+// let a = true;
+
+// a &&= 4;
+// console.log(a)
 
 //??= when the value is null of undefiuned
 
+//comparision operators
+
+//== loose equality, === strict equality with datatypes
+
+// there is no datatype check for greater than and smaller than check
+
+// console.log(true>false)
+// console.log(false>true)
+
+// console.log(5=="5")
+// console.log(5==="5")
+
+// console.log(true=="true")
+// console.log(true==1)
+// console.log(true === 1)
+// console.log(5>=10)
+// console.log(10>=5)
+
+
+//logical operators
