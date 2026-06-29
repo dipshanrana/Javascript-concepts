4f5af33f4b8ba8006b7cdcebbee02798b06051a1
dipshanrana <dipshanranabhat09@gmail.com>
2026-06-29 23:37:16 +0545

    strict completed

---
+++ javascript-learning/strict.js
@@ -0,0 +1,19 @@
+"use strict"
+// // age = 33
+// // name = "dipshan"
+// // console.log(name)
+// // console.log(age)
+
+// let x = 10;
+// delete x;
+
+// console.log(x)
+
+//duplicate parameter
+
+
+function add(a,a){
+    console.log(a)
+}
+
+add(5,11)
